ea4a326be35915fb3f627e5a1e2764fb4b65aa29
rakeshfalke <rfalke@faichi.com>
2013-10-20 11:29:06 +0530

    Added GMT array with template

---
+++ template/advance-clock.tpl.php
@@ -0,0 +1,9 @@
+<div id="<?php echo $variables['images']['id']; ?>" class="clock_container">
+    <div class="lbl"><?php echo str_replace('-', ', ', $variables['images']['id']); ?></div>
+    <div class="clockHolder">
+    <div class="rotatingWrapper"><?php echo $variables['images']['hour']; ?></div>
+    <div class="rotatingWrapper"><?php echo $variables['images']['min']; ?></div>
+    <div class="rotatingWrapper"><?php echo $variables['images']['sec']; ?></div>
+    <?php echo $variables['images']['clock']; ?></div>
+    <div class="digital"> <span class="hr"></span><span class="minute"></span> <span class="period"></span> </div>
+</div>

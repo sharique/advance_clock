55d035ba272c70572545305760b7b956c995a68c
renukakulkarni <renukakulkarni@2602988.no-reply.drupal.org>
2014-01-06 17:04:52 +0530

    Issue #2165789 by renukak: Adding Show only city option to avoid long clock titles.

--- template/digital-clock.tpl.php
+++ template/digital-clock.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Default simple digital clock template to display a list of rows.
@@ -8,14 +7,11 @@
  * @ingroup advance_clock > templates
  */
 ?>
-<?php $clock = $variables['clock'];
-  $location = str_replace('_', ' ', $clock['location']); 
-  $location = str_replace('/', ' / ', $location); 
-?>
+<?php $clock = $variables['clock'];?>
 <div class="container">
   <span class="clock-icon"></span>
   <div class="clock">
-    <div class="advance-clock-location" id="location-<?php print $clock['clock_id']; ?>"><?php print $location; ?></div>
+    <div class="advance-clock-location" id="location-<?php print $clock['clock_id']; ?>"><?php print $clock['location']; ?></div>
     <?php if ($clock['show_date'] == TRUE) : ?>
        <div class="advance-clock-date" id="date-<?php print $clock['clock_id']; ?>"></div>
     <?php endif; ?>

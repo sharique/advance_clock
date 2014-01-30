1b5c0c4a9d8a71010081a99fae34fe6a16e0dbbc
Sharique A. Farooqui <safknw@gmail.com>
2014-01-30 19:05:24 +0530

    fixing a js errro, code formatting

--- template/digital-clock.tpl.php
+++ template/digital-clock.tpl.php
@@ -7,25 +7,25 @@
  * @ingroup advance_clock > templates
  */
 ?>
-<?php $clock = $variables['clock'];?>
+<?php $clock = $variables['clock']; ?>
 <div class="container">
   <span class="clock-icon"></span>
   <div class="clock">
     <div class="advance-clock-location" id="location-<?php print $clock['clock_id']; ?>"><?php print $clock['location']; ?></div>
     <?php if ($clock['show_date'] == TRUE) : ?>
-       <div class="advance-clock-date" id="date-<?php print $clock['clock_id']; ?>"></div>
+      <div class="advance-clock-date" id="date-<?php print $clock['clock_id']; ?>"></div>
     <?php endif; ?>
     <ul class="clock-wrap">
       <li class="hours" id="hours-<?php print $clock['clock_id']; ?>">
       <li id="point">:</li>  
       <li class="min" id="min-<?php print $clock['clock_id']; ?>">
-      <?php if ($clock['show_secs'] == TRUE) : ?>        
-         <li id="point">:</li>
-         <li class="sec" id="sec-<?php print $clock['clock_id']; ?>">
-      <?php endif; ?>
-      <?php if ($clock['format'] == 0) : ?>
-         <li class="format" id="format-<?php print $clock['clock_id']; ?>">
-      <?php endif; ?>
+        <?php if ($clock['show_secs'] == TRUE) : ?>        
+        <li id="point">:</li>
+        <li class="sec" id="sec-<?php print $clock['clock_id']; ?>">
+        <?php endif; ?>
+        <?php if ($clock['format'] == 0) : ?>
+        <li class="format" id="format-<?php print $clock['clock_id']; ?>">
+        <?php endif; ?>
     </ul>
   </div>
 </div>
